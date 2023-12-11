f529f90588d52873b2d8344516a2ad6d42d90089
Mohan Barman <mohan@conscent.ai>
2023-12-11 18:40:52 +0530

    FIX: remove site verification tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ const config = require('./src/config');
 module.exports = {
   siteMetadata: {
     title: 'Mohan Barman',
-    description: '',
+    description: 'Personal portfolio website of Mohan Barman',
     siteUrl: 'https://mohanbarman.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '',
