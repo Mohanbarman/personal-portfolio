e0faa3dc7bfc82e3189b512db2ce2fdd461d856f
Mohan Barman <mohanbarman9711@gmail.com>
2023-12-12 20:46:30 +0530

    Update about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -148,9 +148,9 @@ const About = () => {
           <div>
             <p>
               Hi there! I'm Mohan, and I find joy in crafting digital experiences on the internet.
-              My programming journey began in 2019 during my 11th grade. After completing 12th, I
+              My programming journey began in 2019 during my high schooling and started my professional career in 2020, I
               kickstarted my career as a frontend developer, but soon discovered my true passion for
-              backend applications and servers, and I must add, I love Neovim!
+              backend applications and servers.
             </p>
 
             <p>
